e26c037eca465ec2cdd9821e13dd6063c32ae972
Kirill <yoyohoho84@gmail.com>
2019-10-20 22:24:42 +0300

    Updates

---
+++ precache-manifest.ef23d9aacda199ea96c8ca39260aecd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5148b866c2e9b865166251cac02b944c",
+    "url": "/tztaxnet/index.html"
+  },
+  {
+    "revision": "4dd529ffecd3868b194b",
+    "url": "/tztaxnet/static/css/main.84a29c5a.chunk.css"
+  },
+  {
+    "revision": "c1c8e2a765033d3526c7",
+    "url": "/tztaxnet/static/js/2.b9a99bac.chunk.js"
+  },
+  {
+    "revision": "4dd529ffecd3868b194b",
+    "url": "/tztaxnet/static/js/main.61478988.chunk.js"
+  },
+  {
+    "revision": "13809d5d62500f069080",
+    "url": "/tztaxnet/static/js/runtime-main.c13bb31f.js"
+  }
+]);
